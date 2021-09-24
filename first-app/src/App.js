e14c20beb4d9ba5ecf7b2c799074bae889f92e1a
Hossein Salari <hossein.salari002@gmail.com>
2021-09-24 14:04:23 +0330

    React tutorial

---
+++ first-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="container">
+          <h1>Hossein</h1>
+          <p>react tutorial</p>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
