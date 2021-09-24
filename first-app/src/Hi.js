b08be3bdc092b3da0dd99cb7a1355dc80455f4f1
Hossein Salari <hossein.salari002@gmail.com>
2021-09-24 15:05:46 +0330

    use functional component instead of class component

--- first-app/src/Hi.js
+++ first-app/src/Hi.js
@@ -1,14 +1,23 @@
-import React, {Component} from "react";
+import React from "react";
 
-class Hi extends Component{
-    render(){
-        return (
-            <div>
-                <h1>Hi Hossein</h1>
-                <h3>Welcome to here</h3>
-            </div>
+// class Hi extends Component{
+//     render(){
+//         return (
+//             <div>
+//                 <h1>Hi Hossein</h1>
+//                 <h3>Welcome to here</h3>
+//             </div>
+//         )
+//     }
+// }
+
+const Hi = () => {
+    return (
+        <div>
+            <h1>Hi Hossein</h1>
+            <h3>Welcome to here</h3>
+        </div>
         )
-    }
 }
 
 export default Hi;
