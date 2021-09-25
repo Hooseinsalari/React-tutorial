2457ca0122e93e9677b6ad74a104439937282600
Hossein Salari <hossein.salari002@gmail.com>
2021-09-25 09:47:51 +0330

    Use prevState for update state

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -8,18 +8,29 @@ class App extends Component {
     };
   }
 
-  changeHandler = () => {
-      this.setState({
-          number: this.state.number + 1
-      }, ()=>{console.log(`new number: ${this.state.number}`)})
-      console.log(this.state.number)
+  addOne = () => {
+      // this.setState({
+      //     number: this.state.number + 1
+      // }, ()=>{console.log(`new number: ${this.state.number}`)})
+      // console.log(this.state.number)
+
+      this.setState((prevState) => ({
+        number: prevState.number +1
+      }))
+  }
+
+  addThree = () => {
+    this.addOne();
+    this.addOne();
+    this.addOne();
+    
   }
 
   render() {
     return (
       <>
         <h1>{this.state.number}</h1>
-        <button onClick={this.changeHandler}>add</button>
+        <button onClick={this.addThree}>add3</button>
       </>
     );
   }
