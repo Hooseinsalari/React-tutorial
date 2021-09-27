2cd11fe16bbe8b5fa2af2f0c2119ed65fdb1f777
Hossein Salari <hossein.salari002@gmail.com>
2021-09-27 14:23:45 +0330

    Use setState in class component with out arrow function

--- first-app/src/CalssEvent.js
+++ first-app/src/CalssEvent.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class CalssEvent extends Component {
-    ClickHandler = () => {
-        alert("Click Class")
-    }
+    // ClickHandler = () => {
+    //     alert("Click Class")
+    // }
     
+    constructor(){
+        super();
+        this.state={
+            paragragh: 'text1'
+        }
+        this.ClickHandler = this.ClickHandler.bind(this)
+    }
+
+    ClickHandler() {
+        this.setState({
+            paragragh:"text2"
+        })
+    }
+
     render() {
         return (
             <div>
                 <button onClick={this.ClickHandler}>Class Event</button>
+                <h1>{this.state.paragragh}</h1>
             </div>
         );
     }
