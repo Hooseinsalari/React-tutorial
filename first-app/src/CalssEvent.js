a62c73003890eacec7a80c43bd1de782ce6664fb
Hossein Salari <hossein.salari002@gmail.com>
2021-09-27 20:54:11 +0330

    add a submit butten and use onSubmit event

--- first-app/src/CalssEvent.js
+++ first-app/src/CalssEvent.js
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 
 class CalssEvent extends Component {
-    // ClickHandler = () => {
-    //     alert("Click Class")
-    // }
-    
     constructor(){
         super();
         this.state={
-            text: ''
+            text: '',
+            option: 'option3'
         }
     }
 
     changeHandler = event => {
         this.setState({
-            text : event.target.value
+            text : event.target.value,
+
         })
-        console.log(event.target.value)
+        // console.log(event.target.value)
+    }
+
+    selectHandler = event => {
+        this.setState({
+            option: event.target.value
+        })
+        // console.log(event.target.value)
+    }
+
+    SubmitHandler = event => {
+        event.preventDefault();
+        console.log(this.state.text)
+        console.log(this.state.option)
     }
 
     render() {
+        const {text, option} = this.state;
         return (
-            <div>
-                <input type="text" value={this.state.text} onChange={this.changeHandler}/>
+            <form>
+                <input type="text" value={text} onChange={this.changeHandler}/>
                 <br/>
-                <button onClick={this.ClickHandler}>Class Event</button>
-            </div>
+                <br/>
+                <select value={option} onChange={this.selectHandler}>
+                    <option value="option1">option1</option>
+                    <option value="option2">option2</option>
+                    <option value="option3">option3</option>
+                    <option value="option4">option4</option>
+                </select>
+                <br/>
+                <br/>
+                <button type='submit' onClick={this.SubmitHandler}>Submit</button>
+            </form>
         );
     }
 }
