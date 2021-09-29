a46a73654d7146123e87ff486ca5b0e74570a41a
Hossein Salari <hossein.salari002@gmail.com>
2021-09-29 11:00:56 +0330

    method in props

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,10 +3,22 @@ import React, { Component} from "react";
 import CalssEvent from "./CalssEvent";
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      text: 'Hello'
+    }
+  }
+  
+  sayHi = () => {
+    console.log(this.state.text)
+  }
+  
   render() {
     return(
       <div>
-        <CalssEvent />
+        <CalssEvent sayHi={this.sayHi} />
       </div>
     )
   }
--- first-app/src/CalssEvent.js
+++ first-app/src/CalssEvent.js
@@ -9,44 +9,12 @@ class CalssEvent extends Component {
         }
     }
 
-    changeHandler = event => {
-        this.setState({
-            text : event.target.value,
-
-        })
-        // console.log(event.target.value)
-    }
-
-    selectHandler = event => {
-        this.setState({
-            option: event.target.value
-        })
-        // console.log(event.target.value)
-    }
-
-    SubmitHandler = event => {
-        event.preventDefault();
-        console.log(this.state.text)
-        console.log(this.state.option)
-    }
 
     render() {
-        const {text, option} = this.state;
         return (
-            <form>
-                <input type="text" value={text} onChange={this.changeHandler}/>
-                <br/>
-                <br/>
-                <select value={option} onChange={this.selectHandler}>
-                    <option value="option1">option1</option>
-                    <option value="option2">option2</option>
-                    <option value="option3">option3</option>
-                    <option value="option4">option4</option>
-                </select>
-                <br/>
-                <br/>
-                <button type='submit' onClick={this.SubmitHandler}>Submit</button>
-            </form>
+            <div>
+                <button type='submit' onClick={this.props.sayHi}>Submdit</button>
+            </div>
         );
     }
 }
