0a8ea7062b7ee71a664aee2d23c679af91918200
Hossein Salari <hossein.salari002@gmail.com>
2021-10-08 15:35:50 +0330

    use fetch and get method

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
-import Cars from "./Cars";
-import Motors from "./Motors";
+
 
 class App extends Component {
 
+  constructor(){
+    super();
+    this.state = {
+      postData: []
+    }
+  }
+
+  getData = () =>{
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((json)=> {
+      this.setState({
+        postData: json
+      })
+    });
+  }
 
   render() {
     return (
       <div>
-        <Cars />
-        <Motors />
+        <button onClick={this.getData}>get data</button>
+        <h1>Post:</h1>
+        {this.state.postData.map(post => <p key={post.id}> {post.title}</p>)}
       </div>
     );
   }
