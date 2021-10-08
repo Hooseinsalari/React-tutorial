da93961c904430f735b487557e1ce4eccf49583a
Hossein Salari <hossein.salari002@gmail.com>
2021-10-08 17:05:58 +0330

    axios get

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Post from "./component/Post";
 
 class App extends Component {
   constructor() {
@@ -8,59 +10,25 @@ class App extends Component {
     };
   }
 
-  getData = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts/")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  };
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({
-          postData: json,
-        });
-      });
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => this.setState({
+        postData: response.data
+      }))
   }
 
-  sendPost = () => {
-    const body = JSON.stringify({
-      title: "react tutorial",
-      body: "i am just learning react",
-      userId: 1,
-    });
-
-    const headers = {
-      "Content-type": "application/json; charset=UTF-8",
-    };
-    fetch("https://jsonplaceholder.typicode.com/posts", {
-      method: "POST",
-      body: body,
-      headers: headers,
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  };
-
-  deletePost = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1", {
-      method: "DELETE",
-    })
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-  };
-
+ 
   render() {
+    const { postData } = this.state;
     return (
       <div>
-        <button onClick={this.getData}>get data</button>
+        {/* <button onClick={this.getData}>get data</button>
         <button onClick={this.sendPost}>send post</button>
-        <button onClick={this.deletePost}>delete post</button>
+        <button onClick={this.deletePost}>delete post</button> */}
         <h1>Post:</h1>
-        {this.state.postData.map((post) => (
-          <p key={post.id}> {post.title}</p>
-        ))}
+        {postData.map((post) => <Post key={post.id} title={post.title} body={post.body} />)}
       </div>
     );
   }
