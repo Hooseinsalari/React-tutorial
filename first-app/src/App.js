3cb6725ef1eb2b6b4a8797bf53056085a64c3336
Hossein Salari <hossein.salari002@gmail.com>
2021-10-10 15:22:29 +0330

    redirect

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Route , Switch } from 'react-router-dom';
+import { Route , Switch , Redirect } from 'react-router-dom';
 
 import Navbar from './component/Navbar';
 import Homepage from './component/Homepage';
@@ -8,6 +8,7 @@ import Docs from './component/Docs';
 import Document from './component/Document';
 import Products from './component/Products';
 import Product from './component/Product';
+import NoteFound from './component/NoteFound';
 
 class App extends Component {
   render() {
@@ -20,7 +21,10 @@ class App extends Component {
             <Route path="/Document" component={Document} />
             <Route path="/Docs/:writer?" component={Docs} />
             <Route path="/Products" render={(props) => <Products name="Hossein" {...props} />} />
-            <Route path="/" component={Homepage} />
+            <Route path="/NoteFound" component={NoteFound} />
+            <Redirect from="/crop" to="/Products" />
+            <Route exact path="/" component={Homepage} />
+            <Redirect to="/NoteFound" />
           </Switch>
         </div>
       </div>
