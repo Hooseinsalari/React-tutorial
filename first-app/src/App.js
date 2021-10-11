5ef15f7c9d6cc6a3f031dbd192b3fa3c941caafe
Hossein Salari <hossein.salari002@gmail.com>
2021-10-11 16:11:00 +0330

    objects in use state

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Counter from './components/Counter';
+import Data from './components/Data';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Counter />
+        <Data />
       </div>
     );
   }
