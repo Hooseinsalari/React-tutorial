389bc114dd4d1f30f5882ff7b870f4179b44c934
Hossein Salari <hossein.salari002@gmail.com>
2021-10-11 14:19:28 +0330

    use state

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,32 +1,11 @@
 import React, { Component } from 'react';
-
-import { Route , Switch , Redirect } from 'react-router-dom';
-
-import Navbar from './component/Navbar';
-import Homepage from './component/Homepage';
-import Docs from './component/Docs';
-import About from './component/About';
-import Products from './component/Products';
-import Product from './component/Product';
-import NoteFound from './component/NoteFound';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <div>
-          <Switch>
-            <Route path="/Products/:id" component={Product} />
-            <Route path="/About" component={About} />
-            <Route path="/Docs/:writer?" component={Docs} />
-            <Route path="/Products" render={(props) => <Products name="Hossein" {...props} />} />
-            <Route path="/NoteFound" component={NoteFound} />
-            <Redirect from="/crop" to="/Products" />
-            <Route exact path="/" component={Homepage} />
-            <Redirect to="/NoteFound" />
-          </Switch>
-        </div>
+        <Counter />
       </div>
     );
   }
---
+++ first-app/src/components/Counter.js
@@ -0,0 +1,19 @@
+import React , {useState} from 'react';
+
+const Counter = () => {
+
+    const [number , setNumber] = useState(50);
+
+    const clickHandler = () => {
+        setNumber( number + 1 )
+    }
+
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button onClick={clickHandler}>UP</button>
+        </div>
+    );
+};
+
+export default Counter;
