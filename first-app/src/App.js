62ea6f4f04786679acb10d39bfdcbf4cf0bfd9cc
Hossein Salari <hossein.salari002@gmail.com>
2021-10-12 13:36:01 +0330

    get api in hooks with useEffect

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
-import Logger from './components/Logger';
+import React from 'react';
+import Data from './components/Data';
 
 
 const App = () => {
-  
-  const [isShow, setIsShow] = useState(true)
-  
+ 
   return (
     <div>
-        <button onClick={() => setIsShow(false)}>delete</button>
-        {isShow && <Logger />}
+      <Data />       
     </div>
   );
 };
