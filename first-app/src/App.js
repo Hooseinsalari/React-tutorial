87118ac9e9cc105a5fa2148eb7bfadffde74f276
Hossein Salari <hossein.salari002@gmail.com>
2021-10-13 13:55:40 +0330

    molty reducer

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import NewCounter from './components/NewCounter';
+import Counter from './components/Counter';
 
 const App = () => {
   return (
     <div>
-      <NewCounter />
+      <Counter />
     </div>
   );
 };
