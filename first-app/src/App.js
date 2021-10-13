dacf59f19ad77e23717c56df028529b573c9983c
Hossein Salari <hossein.salari002@gmail.com>
2021-10-13 10:45:33 +0330

    useContext hooks

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import Data from './components/Data';
+import React, {useState} from 'react';
+import ComA from './components/ComA';
 
+export const NameContext = React.createContext();
+export const AgeContext = React.createContext();
 
 const App = () => {
  
+  const [name] = useState("Hossein")
+
   return (
     <div>
-      <Data />       
+        <NameContext.Provider value={name}>
+          <AgeContext.Provider value={19}>
+            <ComA name={name} />
+          </AgeContext.Provider>
+        </NameContext.Provider> 
     </div>
   );
 };
