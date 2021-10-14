e1cee24900205fb443a23d120330334a7eaf13dc
Hossein Salari <hossein.salari002@gmail.com>
2021-10-14 11:19:46 +0330

    useMemo

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import GetData from './components/GetData';
+import React, {useState, useMemo} from 'react';
 
 const App = () => {
+  const [isShown, setIsShown] = useState(true);
+  const [number, setNumber ] = useState(0); 
+
+  const slow = useMemo(() => {
+    for(let i=0; i<100000000; i++) {}
+    return isShown;
+  },[isShown])
+
+  const shownHandler = () => {
+    setIsShown(!isShown)
+  }
+
+  const numberHandler = () => {
+    setNumber((prevnumber) => prevnumber + 1)
+  }
+
   return (
     <div>
-      <GetData />
+      <button onClick={shownHandler}>Shown</button>
+      <h1>{slow? "show" : "hide"}</h1>
+      <h2>{console.log(isShown)}</h2>
+      <br />
+      <button onClick={numberHandler}>+</button>
+      <h1>{number}</h1>
     </div>
   );
 };
